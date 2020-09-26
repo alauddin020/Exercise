f14854b7bc048193dbe8a21eb953bc2596cc7d49
alauddin020 <alauddin020@gmail.com>
2020-09-27 01:13:41 +0600

    Form Factory added

---
+++ database/factories/FormFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Form;
+use Faker\Generator as Faker;
+
+$factory->define(Form::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'number' => $faker->randomNumber(),
+        'email' => $faker->safeEmail,
+        'password' => $faker->randomElement(),
+        'select' => $faker->word,
+        'textarea' => $faker->sentence,
+        'radio' => $faker->randomElement(['male','female']),
+        'color' => $faker->hexColor,
+        'date' => $faker->date('Y-m-d'),
+        'month' => $faker->monthName.' '.$faker->year(),
+        'time' => $faker->time('h:i:s'),
+    ];
+});
